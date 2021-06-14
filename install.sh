17131f49511d6263efc8136387f373055ebf561b
Mitch Vollebregt <31mv9@queensu.ca>
2021-06-14 18:05:24 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
-echo "Creating links to vim.rc, tmux.conf, and .zshrc files"
-ln vimrc ~/.vimrc
-ln .vim ~/.vim
-ln zshrc ~/.zshrc
-ln tmux.conf ~/.tmux.conf
+#echo "Creating links to vim.rc, tmux.conf, and .zshrc files"
+#ln vimrc ~/.vimrc
+#ln .vim ~/.vim
+#ln zshrc ~/.zshrc
+#ln tmux.conf ~/.tmux.conf
 
 #echo "Installing homebrew"
 #/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-echo "Installing packages from brewleaves"
-./brewstaller.sh brewleaves
+#echo "Installing packages from brewleaves"
+#./brewstaller.sh brewleaves
 
 #echo "Fetching dracula theme for iterm"
 #git clone https://github.com/dracula/iterm.git
