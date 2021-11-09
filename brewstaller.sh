9e1be88832dfe19a5e59291fd96231fd53afb1f8
Mitch Vollebregt <mvollebregt@me.com>
2021-11-09 10:09:01 -0500

    borrow cam's config

--- brewstaller.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-for package in $(cat $1); do 
-  brew install $package
-done
-  
--- install.sh
+++ install.sh
@@ -1,14 +1,62 @@
-#echo "Creating links to vim.rc, tmux.conf, and .zshrc files"
-#ln vimrc ~/.vimrc
-#ln .vim ~/.vim
-#ln zshrc ~/.zshrc
-#ln tmux.conf ~/.tmux.conf
+#! /bin/zsh
 
-#echo "Installing homebrew"
-#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+cd "${0%/*}"
 
-#echo "Installing packages from brewleaves"
-#./brewstaller.sh brewleaves
+# Link dotfiles
+#
+echo
+echo Linking dotfiles
+echo ----------------
+echo
 
-#echo "Fetching dracula theme for iterm"
-#git clone https://github.com/dracula/iterm.git
+echo "Creating links to vim.rc, tmux.conf, and .zshrc files"
+
+for dotfile in src/**/*; do
+	destination=~/.${dotfile:4} # Remove src/ and prepend ~/.
+
+	if [ -d "$dotfile" ] && [ ! -d "$destination" ]; then
+		mkdir $destination
+	fi
+
+	if [ -f "$dotfile" ]; then
+		echo "--- $dotfile -> $destination"
+		ln -sf ${dotfile:P} "$destination"
+	fi
+done
+
+# Install dependencies
+#
+echo
+echo Installing dependencies
+echo -----------------------
+echo
+
+function install {
+  if ! command -v name &> /dev/null; then
+    if command -v apt-get &> /dev/null; then
+      sudo apt-get install -y $1
+    elif command -v brew &> /dev/null; then
+      brew install $1
+    fi
+  fi
+  echo "--- $1 installed"
+}
+
+# Neovim
+if command -v apt-get &> /dev/null; then
+  sudo add-apt-repository ppa:neovim-ppa/unstable
+  sudo apt-get update
+fi
+install neovim
+
+curl -fLso ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+nvim --headless +PlugInstall +qall
+echo --- neovim configured
+echo
+
+if [[ $SPIN ]]; then
+  git config --global user.email "mitch.vollebregt@shopify.com"
+  git config --global user.name "Mitch Vollebregt"
+  git config --global --unset-all credential.helper
+fi
